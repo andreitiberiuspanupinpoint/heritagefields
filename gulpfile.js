ba97ceea37d9ea8336813c44f91f54d539d81ba2
andrei spanu design <andrei_spanu@yahoo.com>
2017-05-06 10:44:00 +0100

    Gulpfile.js -> foundation sass

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,26 @@ const reload = require('gulp-livereload');
 
 
 gulp.task('sass', function() {
-    gulp.src('theme/skin/frontend/heritagefields/default/sass/**/*.scss')
-        .pipe(sass())
+    gulp.src('theme/skin/frontend/heritagefields/default/scss/**/*.scss')
+        .pipe(sass({
+            includePaths: ['vendor/bower/foundation-sites/scss']
+        }))
         .pipe(gulp.dest('theme/skin/frontend/heritagefields/default/css/'))
         .pipe(reload());
 });
 
 
+// Copy js
+gulp.task('copyjs', function() {
+    gulp.src('vendor/bower/foundation-sites/dist/js/foundation.min.js')
+        .pipe(gulp.dest('theme/skin/frontend/heritagefields/default/js/'));
+});
+
 
 //Watch task
 gulp.task('watch',function() {
     reload.listen();
-    gulp.watch('theme/skin/frontend/heritagefields/default/sass/**/*.scss',['sass']);
+    gulp.watch('theme/skin/frontend/heritagefields/default/scss/**/*.scss',['sass']);
 });
 
 
